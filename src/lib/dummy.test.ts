f1a55b997b291fdb07fcd9d763e43ac56ebd777d
Bahman Asheghi <Asheghi.sh@gmail.com>
2022-08-29 02:58:08 +0430

    added jest

---
+++ src/lib/dummy.test.ts
@@ -0,0 +1,13 @@
+import { sum } from "./dummy-helper";
+
+describe("dummy test", () => {
+  describe("sum", () => {
+    describe("given two numbers", () => {
+      it("should return sum", () => {
+        const a = 80;
+        const b = 5;
+        expect(sum(a, b)).toBe(a + b);
+      });
+    });
+  });
+});
