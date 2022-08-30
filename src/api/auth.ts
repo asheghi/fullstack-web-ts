c89bc52b51af576a3e5e77bbac5a56788e0fb54f
Bahman Asheghi <Asheghi.sh@gmail.com>
2022-08-31 00:49:15 +0430

    added prisma provider to next-auth

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import GithubProvider from "next-auth/providers/github";
+import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import fetch, { Headers, Request, Response } from "node-fetch";
 import dotenv from "dotenv";
 import { join } from "path";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "../lib/next-auth";
 
 dotenv.config({ path: join(__dirname, "../../.env") });
@@ -21,12 +24,31 @@ if (!globalThis.fetch) {
 const githubSecret = process.env.GITHUB_SECRET!;
 const githubId = process.env.GITHUB_ID!;
 
+const prisma = new PrismaClient();
+
+const providers = [
+  GithubProvider({
+    clientId: githubId,
+    clientSecret: githubSecret,
+  }),
+];
+
+if (process.env.EMAIL_SERVER_HOST) {
+  EmailProvider({
+    server: {
+      host: process.env.EMAIL_SERVER_HOST,
+      port: process.env.EMAIL_SERVER_PORT,
+      auth: {
+        user: process.env.EMAIL_SERVER_USER,
+        pass: process.env.EMAIL_SERVER_PASSWORD,
+      },
+    },
+    from: process.env.EMAIL_FROM,
+  });
+}
+
 export default NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: githubId,
-      clientSecret: githubSecret,
-    }),
-  ],
+  adapter: PrismaAdapter(prisma),
+  providers,
   secret: process.env.JWT_SECRET,
 });
